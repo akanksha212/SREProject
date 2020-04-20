aadb877f4797755c7359abff18fa4e75b56821a0
Shreya Upadhyay <shreya.upadhyay@students.iiit.ac.in>
2020-04-20 23:22:48 +0530

    minor changes

--- videoSummarizer/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/shreya/Desktop/SE_Project/SWEProject/videoSummarizer/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
